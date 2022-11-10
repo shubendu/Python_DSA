a65cc46fdbf6e77184cb09e453c4a520125077f9
Shubendu Biswas <biswasshubendu4@gmail.com>
2022-11-10 10:01:56 +0530

    array day 1

--- array/move_zeros.py
+++ array/move_zeros.py
@@ -1,10 +1,12 @@
 sample = [1,2,0,3,0,4,0]
 
 res = []
-
+count = 0
 for i in sample:
     if i == 0:
+        count = count + 1
         continue
     else:
         res.append(i)
+res = res + [0] * count
 print(res)
